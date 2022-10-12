a10d9db33dcdc58ba40f656568f76ff0d2a9db7e
Hari <114408653+harigih@users.noreply.github.com>
2022-10-12 09:10:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/harigih/LUCIFER.git /LUCIFER
+  git clone https://github.com/harigih/THOR-V3.git /THOR-V3
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LUCIFER
+  git clone $UPSTREAM_REPO /THOR-V3
 fi
 cd /LUCIFER
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "𝐓𝐇𝐎𝐑-𝐕𝟑 𝐒𝐭𝐚𝐫𝐭𝐢𝐧𝐠 𝐁𝐨𝐭⚡...."
 python3 bot.py
